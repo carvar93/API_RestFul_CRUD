de2683bff55776e0247d85591b49f6d257d97e35
Carlos Vargas <cavu93@hotmail.com>
2022-05-04 09:29:23 -0600

    Proyecto api rest, metodos get

---
+++ API_RestFul_CRUD/Controllers/ProductoController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API_RestFul_CRUD.Context;
+using API_RestFul_CRUD.Entities;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API_RestFul_CRUD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductoController : ControllerBase
+    {
+
+        private readonly AppDbContext _context;
+
+
+        public ProductoController(AppDbContext context)
+        {
+            this._context = context;
+        }
+
+
+        // GET: api/<ProductoController>
+        [HttpGet]
+        public IEnumerable<Producto> Get()
+        {
+            return _context.Producto.ToList();
+        }
+
+        // GET api/<ProductoController>/5
+        [HttpGet("{id}")]
+        public Producto Get(string id)
+        {
+            var producto = _context.Producto.FirstOrDefault(p=>p.pro_codigo==id);
+            return producto;
+        }
+
+        // POST api/<ProductoController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ProductoController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ProductoController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
